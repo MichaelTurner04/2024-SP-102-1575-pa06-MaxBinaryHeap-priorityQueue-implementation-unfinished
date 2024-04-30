9fc11444b321ae55eae7177fc9e075e76e67d777
Jack Manhardt <jmmbbv@mst.edu>
2024-04-30 09:22:13 -0500

    init

---
+++ src/my_sort.h
@@ -0,0 +1,13 @@
+#ifndef MY_SORT_H
+#define MY_SORT_H
+#include "MyHeap.h"
+#include "my_is_sorted.h"
+
+// sort the T-array 'data' in-place in decreasing order
+// after the function is finished, the elements of 'data' are sorted in decreasing order
+// hint: start by converting to a heap, then sort using heap operations
+
+template <typename T> void my_sort(T *data, int size) {
+}
+
+#endif
